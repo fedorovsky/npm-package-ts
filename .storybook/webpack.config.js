ca52e491ccb5a79685ab3951a2c2185ca6ab9a5d
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2019-10-18 16:43:35 +0300

    add storysource

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,28 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = ({ config }) => {
   config.resolve.modules = [
-    path.resolve(__dirname, "../src"),
-    ...config.resolve.modules
+    path.resolve(__dirname, '../src'),
+    ...config.resolve.modules,
   ];
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
       {
-        loader: require.resolve("awesome-typescript-loader")
-      }
-    ]
+        loader: require.resolve('awesome-typescript-loader'),
+      },
+    ],
   });
-  config.resolve.extensions.push(".ts", ".tsx");
+  config.module.rules.push({
+    test: /\.stories\.tsx?$/,
+    loaders: [
+      {
+        loader: require.resolve('@storybook/addon-storysource/loader'),
+        options: { parser: 'typescript' },
+      },
+    ],
+    enforce: 'pre',
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
   return config;
 };
